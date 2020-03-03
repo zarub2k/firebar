67a1d902c68e663aa0ced27a57a073518e3b6870
Tham <zarub2k@gmail.com>
2020-03-03 07:12:09 +0530

    first function written

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.ping = functions.https.onRequest(async (_req, _res) => {
+  console.log('Enters ping()');
+  _res.redirect(303, 'Response from ping()');
+});
