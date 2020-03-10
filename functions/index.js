e07d89d8e0327acbbe5b8ccee3c4c986618a285f
Tham <zarub2k@gmail.com>
2020-03-10 06:47:14 +0530

    First firestore function is written

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,9 @@ exports.ping = functions.https.onRequest(async (_req, _res) => {
   console.log('Enters ping()');
   _res.redirect(303, 'Response from ping()');
 });
+
+exports.onCreate = functions.firestore
+  .document('')
+  .onCreate((data, context) => {
+    console.log('Enters onCreate()');
+});
