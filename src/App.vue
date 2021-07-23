0d731dc930a2eaebbb2289e7bd558963ff163de2
Tham <zarub2k@gmail.com>
2021-07-23 23:20:42 +0530

    auth state changed

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
+  <p>Current user: {{ email }}</p>
   <button @click="onSignup">Signup</button>
+  <button @click="onSignin">Signin</button>
 </template>
 
 <script>
+import { ref } from 'vue';
+import { auth } from './services/firebase';
+import { onAuthStateChanged } from "firebase/auth";
+
 import AuthService from './services/AuthService';
 
 export default {
   setup() {
+    let email = ref('')
+    onAuthStateChanged(auth, (user) => {
+      console.log('User auth changed');
+      email.value = user.email;
+    });
+
     const user = {
       email: 'tham@gmail.com',
       password: 'Tham#123'
     };
 
+    AuthService.currentUser()
+    .then((user) => {
+      email.value = user.email;
+    });
+
     const onSignup = () => {
-      AuthService.signupWithEmail(user);
+      AuthService.signUpWithEmail(user);
     };
 
+    const onSignin = () => {
+      AuthService.signInWithEmail(user);
+    }
+
     return {
-      onSignup
+      email,
+      onSignup,
+      onSignin
     }
   },
 }
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,17 +1,33 @@
 import { auth } from './firebase';
+import { onAuthStateChanged } from "firebase/auth";
 import {
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from '@firebase/auth';
 
 export default {
-  async signupWithEmail(user) {
-    console.log('AuthService.signupWithEmail()');
+  async currentUser() {
+    console.log('AuthService.currentUser()')
+    const currentUser = auth.currentUser;
+    console.log(currentUser);
+
+    return currentUser;
+  },
+  async signUpWithEmail(user) {
+    console.log('AuthService.signUpWithEmail()');
     await createUserWithEmailAndPassword(auth, user.email, user.password)
     .then((credential) => {
       console.log(credential.user);
     })
     .catch((error) => {
       console.error(error);
+    })
+  },
+  async signInWithEmail(user) {
+    console.log('AuthService.signInWithEmail');
+    await signInWithEmailAndPassword(auth, user.email, user.password)
+    .then((credential) => {
+      console.log('Auth successful');
     })
   }
 };
